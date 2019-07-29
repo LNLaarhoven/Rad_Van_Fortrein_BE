509e118c9a3efc7768ce12b958d3326200781766
Luca <luca_laarhoven@hotmail.com>
2019-07-29 19:06:16 +0200

    first baby steps

--- backend/src/main/java/radvanfortrein/backend/BackendApplication.java
+++ backend/src/main/java/radvanfortrein/backend/BackendApplication.java
@@ -1,13 +1,48 @@
 package radvanfortrein.backend;
 
+import java.util.Scanner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import radvanfortrein.backend.model.Game;
+import radvanfortrein.backend.model.Inzet;
+import radvanfortrein.backend.model.Station;
+import radvanfortrein.backend.model.Trein;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		//SpringApplication.run(BackendApplication.class, args);
+		Scanner scanner = new Scanner(System.in);
+		
+		Game game = new Game();
+		Station gameStation = game.getStation();
+		Trein gameTrein = game.getTrein();
+		gameStation.getTreinen().add(gameTrein);
+		gameTrein.getStations().add(gameStation);
+		
+		System.out.println("Komt de trein om " + gameTrein.getGeplandeAankomsten().get(0).getHour() + ":"
+				+ gameTrein.getGeplandeAankomsten().get(0).getMinute() + " op station " + gameStation.getNaam() + "? (y/n)");
+		
+		String input = scanner.nextLine();
+		Inzet inzet;
+		
+		if (input.equals("y")) {
+			System.out.println("Hoeveel wil je inzetten?");
+			int inzetBedrag = Integer.parseInt(scanner.nextLine());
+			inzet = new Inzet(game, inzetBedrag, false);
+			game.getInzetten().add(inzet);
+			inzet.getSpeler().getInzetten().add(inzet);
+		} else {
+			System.out.println("Hoeveel wil je inzetten?");
+			int inzetBedrag = Integer.parseInt(scanner.nextLine());
+			inzet = new Inzet(game, inzetBedrag, true);
+			game.getInzetten().add(inzet);
+			inzet.getSpeler().getInzetten().add(inzet);
+		}
+		System.out.println("breakpoint");
 	}
 
 }
--- backend/src/main/java/radvanfortrein/backend/model/Trein.java
+++ backend/src/main/java/radvanfortrein/backend/model/Trein.java
@@ -1,7 +1,9 @@
 package radvanfortrein.backend.model;
 
+import java.sql.Time;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Random;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -20,6 +22,12 @@ public class Trein {
 	private ArrayList<Station> stations;
 
 	public Trein() {
+		this.naam = "test trein";
+		this.geplandeAankomsten = new ArrayList<>();
+		this.werkelijkeAankomsten = new ArrayList<>();
+		this.stations = new ArrayList<>();
+		this.geplandeAankomsten.add(LocalDateTime.now());
+		this.werkelijkeAankomsten.add(this.addRandomTime());
 	}
 	
 	public String getNaam() {
@@ -48,5 +56,22 @@ public class Trein {
 
 	public void setStations(ArrayList<Station> stations) {
 		this.stations = stations;
+	}
+	
+	public LocalDateTime createRandomTime() {
+		Random random = new Random();
+		int millisInDay = 24*60*60;
+		
+		return LocalDateTime.now().plusSeconds(random.nextInt(millisInDay));
+	}
+	
+	public LocalDateTime addRandomTime() {
+		Random random = new Random();
+		int chance = random.nextInt(10);
+		if (chance == 0) {
+			return LocalDateTime.now().plusMinutes(5);
+		} else {
+			return LocalDateTime.now();
+		}
 	}
 }
