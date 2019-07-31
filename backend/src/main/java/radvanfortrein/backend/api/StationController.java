83e47909cda6ed0da5d1c612669d5cbdd4363420
Luca <luca_laarhoven@hotmail.com>
2019-07-31 20:14:14 +0200

    API improvement

--- backend/src/main/java/radvanfortrein/backend/api/StationController.java
+++ backend/src/main/java/radvanfortrein/backend/api/StationController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +19,11 @@ import radvanfortrein.backend.model.Station;
 import radvanfortrein.backend.service.StationService;
 
 @RestController
-@RequestMapping (path = "api/stations")
+@RequestMapping (
+		path = "api/stations",
+		consumes= {MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_PLAIN_VALUE},
+		produces= {MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_PLAIN_VALUE}
+		)
 public class StationController {
 
 	@Autowired
